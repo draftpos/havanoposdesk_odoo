1184a071c330734b4b7af875a2370da98208a887
tatendatembojnr-code <tatendatembojnr@gmail.com>
2026-06-19 07:27:25 +0200

    Enhance product, customer, and sales views and logic

--- sales/models/customer.py
+++ sales/models/customer.py
@@ -1,10 +1,30 @@
-from odoo import models, fields
+from odoo import models, fields, api
+
+class CustomerGroup(models.Model):
+    _name = 'havanoposdesk.customer.group'
+    _description = 'Customer Group'
+
+    name = fields.Char(string='Group Name', required=True)
 
 class Customer(models.Model):
     _name = 'havanoposdesk.customer'
     _description = 'Customer'
 
     name = fields.Char(string='Customer Name', required=True)
+    phone = fields.Char(string='Phone')
+    address = fields.Char(string='Address')
+    city = fields.Char(string='City')
+    country = fields.Char(string='Country')
+    customer_group_id = fields.Many2one('havanoposdesk.customer.group', string='Customer Group')
+    
+    sale_ids = fields.One2many('havanoposdesk.sale', 'customer', string='Sales')
+    balance = fields.Float(string='Balance', compute='_compute_balance', store=False)
+
+    @api.depends('sale_ids.amount_total')
+    def _compute_balance(self):
+        for record in self:
+            record.balance = sum(record.sale_ids.mapped('amount_total'))
+
     customer_type = fields.Selection([
         ('company', 'Company'),
         ('individual', 'Individual'),
