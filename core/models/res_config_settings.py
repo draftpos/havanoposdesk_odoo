55a4717253e5271f1fa47f0ff0c8e1d5e43c8ff1
Josphat Ndhlovu <josphatkndhlovu@gmail.com>
2026-06-23 15:37:47 +0200

    feat:updated settings

--- core/models/res_config_settings.py
+++ core/models/res_config_settings.py
@@ -1,7 +1,19 @@
-from odoo import models, fields
+from odoo import api, models, fields
 
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
+
+    is_super_user = fields.Boolean(
+        string="Is Super User",
+        compute="_compute_is_super_user"
+    )
+
+    @api.depends_context('uid')
+    def _compute_is_super_user(self):
+        is_super = self.env.user.havano_role == 'super_admin' or self.env.user.has_group('base.group_system') or self.env.su
+        for record in self:
+            record.is_super_user = is_super
+
 
     havano_allow_negative_stock = fields.Boolean(
         string="Allow Negative Stock",
