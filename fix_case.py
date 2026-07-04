87fb8ed01d1cadeec26b7f11e062e222d46e1469
tatendatembojnr-code <tatendatembojnr@gmail.com>
2026-07-04 10:26:54 +0200

    Sync final case sensitivity fixes for Windows deployment

---
+++ fix_case.py
@@ -0,0 +1,11 @@
+import glob
+import re
+files = glob.glob('**/*_views.xml', recursive=True)
+for file in files:
+    with open(file, 'r', encoding='utf-8') as f:
+        content = f.read()
+    new_content = content.replace('havanoposdesk_odoo.', 'Havanoposdesk_odoo.')
+    if content != new_content:
+        with open(file, 'w', encoding='utf-8') as f:
+            f.write(new_content)
+print('Fixed module name case in views')
