fe0d71c48c5b05e413cdebc05ac68b6d512121d9
tatendatembojnr-code <tatendatembojnr@gmail.com>
2026-07-09 14:34:15 +0200

    Implement custom centered Lottie loading animation replacing default Odoo loader

--- __manifest__.py
+++ __manifest__.py
@@ -80,6 +80,8 @@
             'havanoposdesk_odoo/static/src/js/router.js',
         ],
         'web.assets_backend': [
+            'havanoposdesk_odoo/static/src/js/lottie-player.js',
+            'havanoposdesk_odoo/static/src/xml/loading_indicator.xml',
             'havanoposdesk_odoo/static/src/js/user_menu_hide.js',
             'havanoposdesk_odoo/static/src/js/whitelabel.js',
             'havanoposdesk_odoo/static/src/scss/custom_theme.scss',
