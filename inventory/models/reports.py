a09ecfe081421f70d714c3e56bb1ce821bd59c37
Josphat Ndhlovu <josphatkndhlovu@gmail.com>
2026-07-11 11:42:41 +0200

    feat:updated purchases api

--- inventory/models/reports.py
+++ inventory/models/reports.py
@@ -11,7 +11,7 @@ class StockValuation(models.Model):
         required=True, 
         default=lambda self: self.env.user.tenant_id.id or (self.env['havanoposdesk.tenant'].search([], limit=1) or self.env['havanoposdesk.tenant'].create({'name': 'Default Tenant'})).id
     )
-    currency_id = fields.Many2one(related='product_id.store_id.currency_id', string='Currency', store=True)
+    currency_id = fields.Many2one(related='tenant_id.currency_id', string='Currency', store=True)
     item_name = fields.Char(related='product_id.name', string='Item Name', store=True)
     item_code = fields.Char(related='product_id.item_code', string='Code', store=True)
     category_id = fields.Many2one(related='product_id.category_id', string='Category', store=True)
@@ -37,7 +37,7 @@ class StockLedger(models.Model):
         required=True, 
         default=lambda self: self.env.user.tenant_id.id or (self.env['havanoposdesk.tenant'].search([], limit=1) or self.env['havanoposdesk.tenant'].create({'name': 'Default Tenant'})).id
     )
-    currency_id = fields.Many2one(related='product_id.store_id.currency_id', string='Currency', store=True)
+    currency_id = fields.Many2one(related='tenant_id.currency_id', string='Currency', store=True)
     item_name = fields.Char(related='product_id.name', string='Item Name', store=True)
     item_code = fields.Char(related='product_id.item_code', string='Code', store=True)
     uom_id = fields.Many2one(related='product_id.uom_id', string='UOM', store=True)
