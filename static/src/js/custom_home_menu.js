9f4a5f03e821adfa350b4801359f286076bf40ad
Josphat Ndhlovu <josphatkndhlovu@gmail.com>
2026-08-21 15:39:47 +0200

    feat:updated and If user pays with cash, balance due should be 0.00....payments must not pass more than the balance due. if user pays specific amount ..all payments must amount to that. Unless user had mode of paymet called on account called User Account. If user uses mode of payment for on accout, paymet status must be partial...that payment mode does not post to payments...it does not receive anything, its just silent. When creating accounts, have a checkbox that is written on account, which should only be used when account type is cash

--- static/src/js/custom_home_menu.js
+++ static/src/js/custom_home_menu.js
@@ -2,30 +2,30 @@
 
 import { NavBar } from "@web/webclient/navbar/navbar";
 import { patch } from "@web/core/utils/patch";
-import { onMounted, onWillUnmount, Component, xml } from "@odoo/owl";
+import { onMounted, onWillUnmount, Component } from "@odoo/owl";
 import { registry } from "@web/core/registry";
 import { useService } from "@web/core/utils/hooks";
 
-// 1. Define the Client Action Component
-class CustomHomeMenuComponent extends Component {
+export class CustomHomeMenuComponent extends Component {
+    static template = "havanoposdesk_odoo.CustomHomeMenu";
+    static props = { "*": true };
+
     setup() {
         this.menuService = useService("menu");
         this.actionService = useService("action");
 
         onMounted(() => {
-            // Hide the Odoo navbar when the home menu is active
             document.body.classList.add("o_home_menu_active");
         });
 
         onWillUnmount(() => {
-            // Restore the navbar when navigating away
             document.body.classList.remove("o_home_menu_active");
         });
     }
 
     get apps() {
         const allApps = this.menuService.getApps();
-        return allApps.filter(app => app.xmlid !== "havanoposdesk_odoo.menu_custom_home_menu_root");
+        return allApps.filter((app) => app.xmlid !== "havanoposdesk_odoo.menu_custom_home_menu_root");
     }
 
     onAppClick(ev, app) {
@@ -34,34 +34,8 @@ class CustomHomeMenuComponent extends Component {
     }
 }
 
-CustomHomeMenuComponent.template = xml`
-    <div class="custom_home_menu_overlay">
-        <div class="custom_home_menu_container">
-            <div class="custom_home_menu_grid">
-                <t t-foreach="apps" t-as="app" t-key="app.id">
-                    <a href="#"
-                       class="custom_home_menu_app_card"
-                       t-on-click="(ev) => this.onAppClick(ev, app)">
-                        <div class="custom_home_menu_app_icon">
-                            <t t-if="app.webIconData">
-                                <img t-att-src="app.webIconData" alt=""/>
-                            </t>
-                            <t t-else="">
-                                <i class="oi oi-apps"></i>
-                            </t>
-                        </div>
-                        <div class="custom_home_menu_app_name"><t t-esc="app.name"/></div>
-                    </a>
-                </t>
-            </div>
-        </div>
-    </div>
-`;
-
-// Register the Client Action
 registry.category("actions").add("custom_home_menu.action", CustomHomeMenuComponent);
 
-// 2. Patch NavBar to replace the apps grid button with a home button
 patch(NavBar.prototype, {
     setup() {
         super.setup();
@@ -71,20 +45,24 @@ patch(NavBar.prototype, {
     },
 
     replaceAppsMenuButton() {
-        const appsMenuContainer = document.querySelector('.o_navbar_apps_menu');
-        if (appsMenuContainer && !appsMenuContainer.classList.contains('custom-replaced')) {
-            appsMenuContainer.classList.add('custom-replaced');
-
-            const buttonHtml = '<button class="custom_home_menu_button border-0 bg-transparent" data-hotkey="h" title="Home Menu"><i class="oi oi-apps"></i></button>';
-            appsMenuContainer.innerHTML = buttonHtml;
-
-            const customButton = appsMenuContainer.querySelector('.custom_home_menu_button');
-            if (customButton) {
-                customButton.addEventListener('click', (e) => {
-                    e.preventDefault();
-                    this.env.services.action.doAction("havanoposdesk_odoo.action_custom_home_menu", { clearBreadcrumbs: true });
-                });
-            }
+        const appsMenuContainer = document.querySelector(".o_navbar_apps_menu");
+        if (!appsMenuContainer || appsMenuContainer.classList.contains("custom-replaced")) {
+            return;
         }
-    }
+        appsMenuContainer.classList.add("custom-replaced");
+
+        const buttonHtml =
+            '<button class="custom_home_menu_button border-0 bg-transparent" data-hotkey="h" title="Home Menu"><i class="oi oi-apps"></i></button>';
+        appsMenuContainer.innerHTML = buttonHtml;
+
+        const customButton = appsMenuContainer.querySelector(".custom_home_menu_button");
+        if (customButton) {
+            customButton.addEventListener("click", (e) => {
+                e.preventDefault();
+                this.env.services.action.doAction("havanoposdesk_odoo.action_custom_home_menu", {
+                    clearBreadcrumbs: true,
+                });
+            });
+        }
+    },
 });
