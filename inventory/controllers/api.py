6a074c0f1a9295564b0acb2def3aa84d096ce766
Josphat Ndhlovu <josphatkndhlovu@gmail.com>
2026-09-09 13:38:56 +0200

    fix: bypass ORM rate field entirely and use raw SQL query to guarantee accurate database value

--- inventory/controllers/api.py
+++ inventory/controllers/api.py
@@ -13,12 +13,13 @@ _logger = logging.getLogger(__name__)
 
 class HavanoPOSDeskAPI(http.Controller):
     def _get_direct_rate(self, env, currency_id, tenant=None):
-        domain = [('currency_id', '=', currency_id)]
         if tenant:
-            domain.append(('tenant_id', '=', tenant.id))
-        rate_record = env['res.currency.rate'].sudo().search(domain, order='name DESC', limit=1)
-        if rate_record:
-            return rate_record.rate or 1.0
+            env.cr.execute("SELECT rate FROM res_currency_rate WHERE currency_id = %s AND tenant_id = %s ORDER BY name DESC LIMIT 1", (currency_id, tenant.id))
+        else:
+            env.cr.execute("SELECT rate FROM res_currency_rate WHERE currency_id = %s ORDER BY name DESC LIMIT 1", (currency_id,))
+        res = env.cr.fetchone()
+        if res and res[0]:
+            return res[0]
         return 1.0
 
 
