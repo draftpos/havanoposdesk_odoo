7fec4bb1126acd355c5ed65805be6a9f070ae473
tatendatembojnr-code <tatendatembojnr@gmail.com>
2026-09-22 11:31:19 +0200

    feat: complete whitelabeling for offline page and custom database manager screen

--- __manifest__.py
+++ __manifest__.py
@@ -40,6 +40,7 @@
         'core/views/auth_templates.xml',
         'core/views/error_log_views.xml',
         'core/views/support_ticket_views.xml',
+        'core/views/database_manager_views.xml',
 
         'accounts/views/account_views.xml',
         'accounts/views/expense_views.xml',
